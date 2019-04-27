97e7a01cc1bd64d068511cf22e9e40ae39480a82
Faisal Animashaun <d.f.animashaun@gmail.com>
2019-04-26 21:04:35 -0400

    added some comments and scaffolding methods for the students

--- PlayerAI.py
+++ PlayerAI.py
@@ -11,8 +11,19 @@ def log2(x):
         return 0
     return math.log(x, 2)
 
+"""
+This class stores the move that was made and the resulting utility.
+"""
 class Result(object):
+    """
+    Args:
 
+        move: (int) represents the move that was made. 1 = up, 2 = down,
+        3 = left, 4 = right.
+
+        utility: (int) THe utility of the resulting state after applying the
+        move
+    """
     def __init__(self, move=None, utility=None):
         self.move = move
         self.utility = utility
@@ -23,33 +34,110 @@ class Result(object):
 class PlayerAI(BaseAI):
 
     MAX_DEPTH = 14
-    # the actual time limit is 0.2 but we set it lower so we have time to
-    # return a result
-    TIME_LIMIT = 0.18
 
+    """
+    Returns a move for the current grid state. This method is called in
+    GameManager.py.
+    """
     def getMove(self, grid):
-        return self.alpha_beta_search(grid);
+        return self.minimax(grid);
 
-    def alpha_beta_search(self, grid):
+
+    """
+    Should implement the minimax algorithm to return a move for the given state.
+    """
+    def minimax(self, grid):
+        return self.random_move(grid)
+
+
+    """
+    Returns a random move for the given grid.
+    """
+    def random_move(self, grid):
         return random.choice(self.get_moves_for_max(grid))
 
-    def max_value(self, grid, alpha=None, beta=None, depth=None, previous=None):
+
+    """
+    Returns the best move that max can make.
+    """
+    def max_value(self, grid, depth=None, previous=None):
+        """
+        1.) Check if the grid is in a terminal state. If it is then return a
+        Result object with the utility of the current state and the move that
+        caused it.
+
+        2.) Get all the moves that max(our bot in this case) can make. Make sure
+        you clone the grid before applying the move.
+
+        3.) For every one of those moves see what min would do.
+
+        4.) Select the move with the highest value.
+        """
         pass
 
-    def min_value(self, grid, alpha=None, beta=None, depth=None, previous=None):
-        pass
 
-    def terminal_test(self, grid, depth):
-        if time.clock() - self.start_time > self.TIME_LIMIT:
-            return True
-        if not grid.canMove():
-            return True
-        return depth > self.MAX_DEPTH
-
-    def utility(self, grid):
-        pass
-
+    """
+    Returns the moves that max can make and the resulting grid.
+    """
     def get_moves_for_max(self, grid):
-        return grid.getAvailableMoves()
+        pass
+
+
+    """
+    Returns the best move that min can make. You can think of this as the worst
+    move min can make to hinder max.
+    """
+    def min_value(self, grid, depth=None, previous=None):
+         """
+        1.) Check if the grid is in a terminal state. If it is then return a
+        Result object with the utility of the current state and the move that
+        caused it.
+
+        2.) Get all the moves that min(our bots adversary) can make. What
+        constitutes a "move" for min though? Hint: Remember that a new tile is
+        added to the board....
+        Like above remember to clone the board before applying the "move".
+
+        3.) For every one of those moves see what max(our bot) would do.
+
+        4.) Select the move with the lowest value.
+        """
+        pass
+
+    def get_moves_for_min
+
+
+    """
+    Determines if the grid is in a terminal state. Specifically this method
+    terminates there are no legal moves or if the algorithm has decended to
+    the maximum depth.
+
+    Args:
+        grid: (Grid) The current state of the grid
+        depth: (int) the current depth of this branch of the search tree.
+    """
+    def terminal_test(self, grid, depth):
+        return grid.canMove() or depth > self.MAX_DEPTH
+
+
+    """
+    Returns the utility for the current state of the grid. The current
+    implementation simply optmizes for the largest value. Can you come up with a
+    smarter utility function(or set of utility functions...hint hint :)
+
+    Args:
+        grid: (Grid) the current state of the grid.
+    """
+    def utility(self, grid):
+        return max_tile(tile)
+
+    """
+    Returns the value of the largest tile in the grid.
+
+    Args:
+        grid: (Grid) the current state of the grid.
+    """
+    def max_tile(self, grid):
+        return log2(grid.getMaxTile())
 
 
